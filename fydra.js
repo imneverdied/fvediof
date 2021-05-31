ec7df0b9c6fb60448ff17c4faddf6d4815e0f500
imneverdied <imneverdied@gmail.com>
2021-05-31 23:05:10 +0800

    1 file modified, 1 deleted

--- fydra.js
+++ fydra.js
@@ -1,87 +1,133 @@
-var _0x3637 = [
-  "428882fHiVbK",
-  "2gWgnBf",
-  "7obCmUE",
-  "435468xNIPmx",
-  "replace",
-  "\x5cw+",
-  "1tWLjeg",
-  "716932xXUZAR",
-  "toString",
-  "26611fbHVlg",
-  "3\x206=[],c=[],7=[],d=[],f=[],g=[],h=[],k=[],l=[],m=[];4\x20u(E){3\x20t=$(v).5()+\x22\x20\x22+E;$(o).5(t)}4\x20w(E){3\x20t=[],e=[],L=[],A=[],a=[],i=[],T=[],x=[];8(j=0;j<7.p;j++)J(E==7[j]){t.2(7[j]),e.2(d[j]),L.2(f[j]),A.2(g[j]),a.2(h[j]),i.2(k[j]),T.2(l[j]),x.2(m[j]);3\x20n=t[0]+\x22\x20第1集\x22;$(v).5(t[0]),$(K).5(L[0]),$(o).5(n),q(i[0])}}4\x20q(E){3\x20t=\x22\x22;8(i=0;i<E;i++)t=t+\x22<9>第\x22+(i+1)+\x22集</9>\x5cr\x5cn\x22;3\x20e=\x27<b\x20y\x20=\x22z\x22>\x5cr\x5cn\x27+t+\x22</b>\x22;$(z).B(e)}4\x20s(){3\x20E,t=(M\x20N).O();t=C(t);3\x20e=P(s,Q)}4\x20C(E){3\x20t=\x22🌑\x22;8(j=0;j<E;j++)t.p<R&&(\x22🌑\x22==t?t=\x22🌒\x22:\x22🌒\x22==t?t=\x22🌓\x22:\x22🌓\x22==t?t=\x22🌔\x22:\x22🌔\x22==t?t=\x22🌕\x22:\x22🌕\x22==t?t=\x22🌖\x22:\x22🌖\x22==t?t=\x22🌗\x22:\x22🌗\x22==t?t=\x22🌘\x22:\x22🌘\x22==t&&(t=\x22🌑\x22));$(S).5(t)}$(D).U(4(){s(),$.V({W:!1}),$.X(\x22Y://Z.10.11/12/13.14\x22,4(E){15=E,$.16(E,4(E,t){6.2(t.F),c.2(t.G),7.2(t.F),d.2(t.17),f.2(t.18),g.2(t.19),h.2(t.1a),k.2(t.G),l.2(t.1b),m.2(t.1c)});3\x20t=\x22\x22;8(i=0;i<6.p;i++)t=t+\x22<9>\x22+6[i]+\x22</9>\x5cr\x5cn\x22;3\x20e=\x27<b\x20y\x20=\x22H\x22>\x5cr\x5cn\x27+t+\x22</b>\x22;$(H).B(e),q(c[0]),w(6[0]),u(\x22第1集\x22)})}),$(I).1d(4(){3\x20E=D.1e(\x22I\x22).1f;$(o).5(E)});",
-  "2135637AFmmuQ",
-  "||push|var|function|text|play_NAME|ALL_NAME|for|option||select|first_EPISO|ALL_RNAME||ALL_NBRTYPE|ALL_PATH|ALL_SEASON|||ALL_EPISODE|ALL_EXT|ALL_CC||TEXT2|length|EPISODEmaker||startTime||eSLECT|NAME_NOW|Fsearch|_|id|TEXT4||html|checkTime|document||NAME|EPISODE|TEXT3|SELECT|if|TYPE_NOW||new|Date|getSeconds|setTimeout|500|30|load||ready|ajaxSetup|cache|getJSON|https|imneverdied|github|io|jmespath|example|json|Gdata|each|RNAME|NBRTYPE|PATH|SEASON|EXT|CC|keyup|getElementById|value",
-  "1028138iUrnBa",
-  "54852uCOPAX"
-];
-var _0x268e63 = _0x906d;
-function _0x906d(_0x3e3948, _0x4653e7) {
-  _0x3e3948 = _0x3e3948 - 0x92;
-  var _0x3637dc = _0x3637[_0x3e3948];
-  return _0x3637dc;
+var play_NAME = [];
+var first_EPISO = [];
+//json檔取得的所有NAME => ALL_NAME
+var ALL_NAME = [];
+var ALL_RNAME = [];
+var ALL_NBRTYPE = [];
+var ALL_PATH = [];
+var ALL_SEASON = [];
+var ALL_EPISODE = [];
+var ALL_EXT = [];
+var ALL_CC = [];
+
+$(document).ready(function() {
+  startTime();
+  $.ajaxSetup({ cache: false });
+  $.getJSON('https://imneverdied.github.io/jmespath/example.json', function(
+    data
+  ) {
+    Gdata = data;
+
+    /* loop through array */
+    $.each(data, function(index, d) {
+      play_NAME.push(d.NAME);
+      first_EPISO.push(d.EPISODE);
+
+      ALL_NAME.push(d.NAME);
+      ALL_RNAME.push(d.RNAME);
+      ALL_NBRTYPE.push(d.NBRTYPE);
+      ALL_PATH.push(d.PATH);
+      ALL_SEASON.push(d.SEASON);
+      ALL_EPISODE.push(d.EPISODE);
+      ALL_EXT.push(d.EXT);
+      ALL_CC.push(d.CC);
+    });
+    //$("#TEXT").text(play_NAME.join(''))
+
+    //讀jsonNAME生下拉選單====
+    var selection = '';
+    for (i = 0; i < play_NAME.length; i++) {
+      selection = selection + '<option>' + play_NAME[i] + '</option>\r\n';
+    }
+    var mix = '<select id ="TEXT3">\r\n' + selection + '</select>';
+    $(TEXT3).html(mix);
+    EPISODEmaker(first_EPISO[0]);
+    Fsearch(play_NAME[0]);
+    eSLECT('第1集');
+
+    //========================
+  });
+});
+
+function eSLECT(value) {
+  var strRTN = $(NAME_NOW).text() + ' ' + value;
+  $(TEXT2).text(strRTN);
 }
-(function(_0x3cf021, _0x106808) {
-  var _0x26c319 = _0x906d;
-  while (!![]) {
-    try {
-      var _0x2279e5 =
-        parseInt(_0x26c319(0x95)) +
-        parseInt(_0x26c319(0xa0)) +
-        parseInt(_0x26c319(0x9e)) * -parseInt(_0x26c319(0x9c)) +
-        -parseInt(_0x26c319(0x9f)) * -parseInt(_0x26c319(0x97)) +
-        parseInt(_0x26c319(0x9b)) +
-        -parseInt(_0x26c319(0x94)) * -parseInt(_0x26c319(0x9d)) +
-        -parseInt(_0x26c319(0x99));
-      if (_0x2279e5 === _0x106808) break;
-      else _0x3cf021["push"](_0x3cf021["shift"]());
-    } catch (_0x1fd36a) {
-      _0x3cf021["push"](_0x3cf021["shift"]());
+
+function Fsearch(value) {
+  //現在選擇的影集 => F_NAME
+  var F_NAME = [];
+  var F_RNAME = [];
+  var F_NBRTYPE = [];
+  var F_PATH = [];
+  var F_SEASON = [];
+  var F_EPISODE = [];
+  var F_EXT = [];
+  var F_CC = [];
+
+  /* loop through array */
+
+  for (j = 0; j < ALL_NAME.length; j++)
+    if (value == ALL_NAME[j]) {
+      //過濾資料
+      //取得現在選擇的影集所有欄位資料
+      F_NAME.push(ALL_NAME[j]);
+      F_RNAME.push(ALL_RNAME[j]);
+      F_NBRTYPE.push(ALL_NBRTYPE[j]);
+      F_PATH.push(ALL_PATH[j]);
+      F_SEASON.push(ALL_SEASON[j]);
+      F_EPISODE.push(ALL_EPISODE[j]);
+      F_EXT.push(ALL_EXT[j]);
+      F_CC.push(ALL_CC[j]);
+
+      //var strRTN = F_NAME[0] + F_NBRTYPE[0];
+      var strRTN = F_NAME[0] + ' 第1集';
+      $(NAME_NOW).text(F_NAME[0]);
+      $(TYPE_NOW).text(F_NBRTYPE[0]);
+      $(TEXT2).text(strRTN);
+      EPISODEmaker(F_EPISODE[0]);
+    }
+}
+
+function EPISODEmaker(value) {
+  //讀jsonNAME生集數下拉選單========
+  var selection = '';
+  for (i = 0; i < value; i++) {
+    selection = selection + '<option>第' + (i + 1) + '集</option>\r\n';
+  }
+  var mix = '<select id ="TEXT4">\r\n' + selection + '</select>';
+  $(TEXT4).html(mix);
+  //========================
+}
+
+$(SELECT).keyup(function() {
+  var AAA = document.getElementById('SELECT').value;
+  $(TEXT2).text(AAA);
+});
+
+function startTime() {
+  var today = new Date();
+  var ss = today.getSeconds();
+  ss = checkTime(ss);
+
+  var timeoutId = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  var TT = '🌑';
+
+  for (j = 0; j < i; j++) {
+    if (TT.length < 30) {
+      if (TT == '🌑') TT = '🌒';
+      else if (TT == '🌒') TT = '🌓';
+      else if (TT == '🌓') TT = '🌔';
+      else if (TT == '🌔') TT = '🌕';
+      else if (TT == '🌕') TT = '🌖';
+      else if (TT == '🌖') TT = '🌗';
+      else if (TT == '🌗') TT = '🌘';
+      else if (TT == '🌘') TT = '🌑';
     }
   }
-})(_0x3637, 0x865d4),
-  eval(
-    (function(
-      _0x5bd6c4,
-      _0x280301,
-      _0x1df0a7,
-      _0x67066f,
-      _0xa00757,
-      _0xe67a04
-    ) {
-      var _0x51356d = _0x906d;
-      _0xa00757 = function(_0x5abf86) {
-        var _0x1e69c6 = _0x906d;
-        return (
-          (_0x5abf86 < _0x280301
-            ? ""
-            : _0xa00757(parseInt(_0x5abf86 / _0x280301))) +
-          ((_0x5abf86 = _0x5abf86 % _0x280301) > 0x23
-            ? String["fromCharCode"](_0x5abf86 + 0x1d)
-            : _0x5abf86[_0x1e69c6(0x96)](0x24))
-        );
-      };
-      if (!""["replace"](/^/, String)) {
-        while (_0x1df0a7--)
-          _0xe67a04[_0xa00757(_0x1df0a7)] =
-            _0x67066f[_0x1df0a7] || _0xa00757(_0x1df0a7);
-        (_0x67066f = [
-          function(_0x1509bc) {
-            return _0xe67a04[_0x1509bc];
-          }
-        ]),
-          (_0xa00757 = function() {
-            var _0x27316f = _0x906d;
-            return _0x27316f(0x93);
-          }),
-          (_0x1df0a7 = 0x1);
-      }
-      while (_0x1df0a7--)
-        if (_0x67066f[_0x1df0a7])
-          _0x5bd6c4 = _0x5bd6c4[_0x51356d(0x92)](
-            new RegExp("\x5cb" + _0xa00757(_0x1df0a7) + "\x5cb", "g"),
-            _0x67066f[_0x1df0a7]
-          );
-      return _0x5bd6c4;
-    })(_0x268e63(0x98), 0x3e, 0x4e, _0x268e63(0x9a)["split"]("|"), 0x0, {})
-  );
+
+  $(load).text(TT);
+}
